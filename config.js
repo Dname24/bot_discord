6a4379437a6e203c6ee5b02357860d173203f13d
DiegoEd <71272778+Dname24@users.noreply.github.com>
2021-11-09 17:49:41 -0300

    archivos para el bot

---
+++ config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    /** Bot owner(s)  */
+    owner: ['253006486349938688'],
+
+    /** Bot admin user ids, can reboot the bot and inherits support permissions */
+    admins: [''],
+
+    /** Bot support user ids, can reload commands, display bot stats and inherits server owner permissions */
+    support: [''],
+
+    /** Bot support server invite link */
+    invite: 'link',
+
+    /** Bot command prefix */
+    prefix: '!',
+
+    /** Error logging channel */
+    errorLogChannel: 'channel'
+};
